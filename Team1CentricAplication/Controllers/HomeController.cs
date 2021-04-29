60cdc6aafd1e3edcb9cad1efcd04c928708be0ff
Alex Trombold <at354216@ohio.edu>
2021-04-29 11:35:03 -0400

    leaderboards and most recent done

--- Team1CentricAplication/Controllers/HomeController.cs
+++ Team1CentricAplication/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace Team1CentricAplication.Controllers
         public ActionResult Index()
         {
             var values = db.Values;
+            var count = db.Profiles.Include(p => p.AwardRecipient).Count();
           
             //var top10 = (from v in valuesList
             //             group v by v.AwardNominee into g
@@ -32,7 +33,7 @@ namespace Team1CentricAplication.Controllers
             //                    let count = g.Count()
             //                    orderby count descending
             //                    select new { Value = g.Key, Count = count }).Take(10);
-            var Excellence10 = db.Profiles.Include(p => p.AwardRecipient).OrderByDescending(p => p.AwardRecipient.Count()).Take(10);
+            var Excellence10 = db.Values.Where(v => v.nominatedValues == Values.CoreValue.Excellence).Include(v => v.AwardNominee).OrderByDescending(o => o.recognizationDate).Take(10);
 
             //var Integrity10 = (from v in valuesList
             //                 where v.nominatedValues == Values.CoreValue.Integrity
@@ -40,31 +41,31 @@ namespace Team1CentricAplication.Controllers
             //             let count = g.Count()
             //           orderby count descending
             //         select new { Value = g.Key, Count = count }).Take(10);
-            var Integrity10 = db.Profiles.Include(p => p.AwardRecipient).OrderByDescending(p => p.AwardRecipient.Count()).Take(10);
+            var Integrity10 = db.Values.Where(v => v.nominatedValues == Values.CoreValue.Integrity).Include(v => v.AwardNominee).OrderByDescending(o => o.recognizationDate).Take(10);
             //var Stewardship10 = (from v in valuesList
             //                   where v.nominatedValues == Values.CoreValue.Stewardship
             //                 group v by v.AwardNominee into g
             //               let count = g.Count()
             //             orderby count descending
             //           select new { Value = g.Key, Count = count }).Take(10);
-            var Stewardship10 = db.Profiles.Include(p => p.AwardRecipient).OrderByDescending(p => p.AwardRecipient.Count()).Take(10);
+            var Stewardship10 = db.Values.Where(v => v.nominatedValues == Values.CoreValue.Stewardship).Include(v => v.AwardNominee).OrderByDescending(o => o.recognizationDate).Take(10);
             //var Innovate10 = (from v in valuesList
             //                where v.nominatedValues == Values.CoreValue.Innovate
             //              group v by v.AwardNominee into g
             //            let count = g.Count()
             //          orderby count descending
             //        select new { Value = g.Key, Count = count }).Take(10);
-            var Innovate10 = db.Profiles.Include(p => p.AwardRecipient).OrderByDescending(p => p.AwardRecipient.Count()).Take(10);
+            var Innovate10 = db.Values.Where(v => v.nominatedValues == Values.CoreValue.Innovate).Include(v => v.AwardNominee).OrderByDescending(o => o.recognizationDate).Take(10);
             //var Balance10 = (from v in valuesList
             //               where v.nominatedValues == Values.CoreValue.Balance
             //             group v by v.AwardNominee into g
             //           let count = g.Count()
             //         orderby count descending
             //       select new { Value = g.Key, Count = count }).Take(10);
-            var Balance10 = db.Profiles.Include(p => p.AwardRecipient).OrderByDescending(p => p.AwardRecipient.Count()).Take(10);
+            var Balance10 = db.Values.Where(v => v.nominatedValues == Values.CoreValue.Balance).Include(v => v.AwardNominee).OrderByDescending(o => o.recognizationDate).Take(10);
 
-          
-            ViewBag.top10 = top10.ToList(); 
+
+            ViewBag.top10 = top10.ToList();
             ViewBag.excellence = Excellence10.ToList();
             ViewBag.integrity = Integrity10.ToList();
             ViewBag.stewardship = Stewardship10.ToList();
